260164e273e9cce06b37304e4bc7385af2bd5900
Joonas Yliaho <joonas.yliaho@gmail.com>
2019-03-01 00:47:41 +0200

    explanation for build.js

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,14 @@
+/**
+ * Why use a custom builder script instead of just using the CLI
+ * you may ask? Well, turns out that if you want to keep using a
+ * custom domain on github pages, you sorta need to have CNAME file
+ * included in your gh-pages branch instead of just setting the
+ * custom domain from github repo's settings. Each consequent push
+ * to the gh-pages branch removes whatever you inputted as your
+ * custom domain from the settings page. Bundling the source to
+ * a production ready code and _then_ creating the CNAME file is
+ * a quick and dirty workaround this.
+ */
 const Bundler = require('parcel-bundler')
 const path = require('path')
 const { createCNAMEFile } = require('./utils/index')
